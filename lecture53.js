82959a13abfd51150f59c3c80252ddfc23dbfb04
FirozuiShaker <furouzi.abolfazl@yahoo.com>
2023-09-30 15:31:29 +0200

    L53 P2 show some content in h2

--- lecture53.js
+++ lecture53.js
@@ -1,2 +1,13 @@
 //DOM manipulation
-console.log(document.getElementById("title"));
+//console.log(document.getElementById("title"));
+
+//console.log(document instanceof HTMLDucument);
+
+function sayHello(){
+var name =
+        document.getElementById("name").value;
+        var message="Hello " + name +"!";
+
+        document.getElementById("content")
+        .textContent=message;
+}
